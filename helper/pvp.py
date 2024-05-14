e75e7a96c0b35e0e0de618bb50cbeaf3ef434454
justin <z5424659@ad.unsw.edu.au>
2024-05-14 12:33:06 +1000

    Helper function steal

--- helper/pvp.py
+++ helper/pvp.py
@@ -1,8 +1,35 @@
 from data import save, load, check_usr, log
 import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_ID = int(os.getenv('BOT_ID'))
+
 
 async def battle_helper(ctx, data, user, amount):
     pass
 
-async def steal_helper(ctx, data, user):
-    pass
+async def steal_helper(ctx, data):
+    log('------\nsteal', ctx.author.global_name, data)
+    chosen = randomly_choose_user_in_guild(data, ctx)
+    if random.random() < 0.5 and chosen != str(ctx.author.id):
+        chosen_name = data[chosen]['name']
+        user_name = data[str(ctx.author.id)]['name']
+        amount_to_steal = 0.05 * data[chosen]['balance']
+        data[chosen]['balance'] -= amount_to_steal
+        data[str(ctx.author.id)]['balance'] += amount_to_steal
+        await ctx.send(f'You stole ${amount_to_steal:.2f} from **{chosen_name}**\n ~ **{user_name}** now has ${data[str(ctx.author.id)]["balance"]:.2f}\n ~ **{chosen_name}** now has ${data[chosen]["balance"]:.2f}')
+    else:
+        await ctx.send('You failed to steal from anyone')
+    save(data)
+    log('------\nsteal(success)', ctx.author.global_name, data)
+
+def randomly_choose_user_in_guild(data, ctx):
+    member_ids = [member.id for member in ctx.guild.members]
+    member_ids.remove(BOT_ID)
+    chosen = str(random.choice(member_ids))
+    while chosen not in data or data[chosen]['balance'] == 0:
+        chosen = str(random.choice(member_ids))
+    return chosen
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from data import save, load, check_usr, log
 from importlib import import_module
 from helper.util import balance_helper, poor_helper, shutdown_helper, leaderboard_helper
 from helper.flip import flip_helper
+from helper.pvp import battle_helper, steal_helper
 import datetime
 import re
 
@@ -66,27 +67,7 @@ async def battle(ctx, user: discord.User, amount: float = 0):
 @commands.cooldown(1, 3600, commands.BucketType.user)
 @bot.command(help='chance to steal a portion of another user\'s balance')
 async def steal(ctx):
-    log('------\nsteal', ctx.author.global_name, data)
-    chosen = randomly_choose_user_in_guild(data, ctx)
-    if random.random() < 0.5 and chosen != str(ctx.author.id):
-        chosen_name = data[chosen]['name']
-        user_name = data[str(ctx.author.id)]['name']
-        amount_to_steal = 0.05 * data[chosen]['balance']
-        data[chosen]['balance'] -= amount_to_steal
-        data[str(ctx.author.id)]['balance'] += amount_to_steal
-        await ctx.send(f'You stole ${amount_to_steal:.2f} from **{chosen_name}**\n ~ **{user_name}** now has ${data[str(ctx.author.id)]["balance"]:.2f}\n ~ **{chosen_name}** now has ${data[chosen]["balance"]:.2f}')
-    else:
-        await ctx.send('You failed to steal from anyone')
-    save(data)
-    log('------\nsteal(success)', ctx.author.global_name, data)
-
-def randomly_choose_user_in_guild(data, ctx):
-    member_ids = [member.id for member in ctx.guild.members]
-    member_ids.remove(BOT_ID)
-    chosen = str(random.choice(member_ids))
-    while chosen not in data or data[chosen]['balance'] == 0:
-        chosen = str(random.choice(member_ids))
-    return chosen
+    await steal_helper(ctx, data)
 
 @bot.event
 async def on_command_error(ctx, error):
