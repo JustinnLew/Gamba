0959d913f67f47f0fa6c791e944f5d4c6096c8df
justin <z5424659@ad.unsw.edu.au>
2024-05-16 14:46:31 +1000

    Improved loggin with json string compression

---
+++ decompress.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import json
+import gzip
+import base64
+import pprint
+
+def decompress_json(compressed_base64):
+    # Decode the base64 string to get the compressed bytes
+    compressed_bytes = base64.b64decode(compressed_base64)
+
+    # Decompress the bytes using gzip
+    json_str = gzip.decompress(compressed_bytes).decode('utf-8')
+
+    # Convert the JSON string back to a JSON object
+    data = json.loads(json_str)
+
+    return data
+
+if __name__ == '__main__':
+    # Example compressed base64 string
+    input = input("Enter the compressed base64 string: ")
+    pprint.pprint(decompress_json(input))
