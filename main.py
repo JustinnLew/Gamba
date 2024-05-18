04d1fe6fde16667e352a4fd8166a707dd85aac46
justin <z5424659@ad.unsw.edu.au>
2024-05-19 01:41:29 +1000

    Gamba mostly complete

--- main.py
+++ main.py
@@ -62,12 +62,12 @@ async def on_command_error(ctx, error):
         time = re.sub(r'.\d*$', '', time)
         await ctx.send(f'This command is on cooldown, you can use it in {time}')
     if isinstance(error, commands.DisabledCommand):
-        await ctx.send('Under maintenence')
+        await ctx.send('Watch the game')
 
 # -------------------------------------------- COMMANDS --------------------------------------------
 
 @bot.command(name='gamba-create', help='Create a gamba', enabled = ENABLED)
-async def gamba_create(ctx, *args):
+async def gamba_create(ctx, duration: int, *args):
     name = ' '.join(args)
     if data["gamba"]:
         await ctx.send('Gamba already in progress')
@@ -79,14 +79,14 @@ async def gamba_create(ctx, *args):
             "name": ctx.author.global_name or ctx.author.name
         },
         "name": name,
-        "yes": {},
-        "no": {}
+        "votes": [],
     }
+    save(data)
     await ctx.send(f'```fix\nGamba Created: {name}```')
     await ctx.send(f"```yaml\n!gamba-yes <amount> to vote yes\n```")
     await ctx.send(f"```yaml\n!gamba-no <amount> to vote no\n```")
-    await ctx.send(f"```md\n> Betting ends in 30 seconds\n```")
-    await sleep(30)
+    await ctx.send(f"```md\n> Betting ends in {duration} seconds\n```")
+    await sleep(duration)
     data["gamba"]["state"] = "closed"
     await ctx.send(f"```md\n> Betting closed\n```")
 
@@ -107,16 +107,124 @@ async def view_gamba(ctx):
     embed.add_field(name='Owner', value=data["gamba"]["owner"]["name"])
     embed.add_field(name='Name', value=data["gamba"]["name"])
     embed.add_field(name='State', value=data["gamba"]["state"])
+    yes = ""
+    no = ""
+    for v in data["gamba"]["votes"]:
+        if v["choice"] == "yes":
+            yes += f'{v["name"]}: {v["amount"]}\n'
+        else:
+            no += f'{v["name"]}: {v["amount"]}\n'
+    embed.add_field(name='Yes', value=yes)
+    embed.add_field(name='No', value=no)
     await ctx.send(embed=embed)
 
-# TODO
-# @bot.command(name='gamba-yes', help='Bet on yes', enabled = ENABLED)
-# @bot.command(name='gamba-no', help='Bet on no', enabled = ENABLED)
-# @bot.command(name='gamba-end', help='End gamba', enabled = ENABLED)
+@bot.command(name='gamba-yes', help='Bet on yes', enabled = ENABLED)
+async def gamba_yes(ctx, amount):
+    name = ctx.author.global_name or ctx.author.name
+    if data["gamba"]["state"] != "betting":
+        await ctx.send('Betting is closed')
+        return
+    if amount == 'all' or amount == 'ALL':
+        amount = float(data["users"][str(ctx.author.id)]['balance'])
+    else:
+        amount = float(amount)
+    if data["users"][str(ctx.author.id)]['balance'] < amount:
+        await ctx.send('Not enough coins')
+        return
+    for v in data["gamba"]["votes"]:
+        if v["name"] == name:
+            data["users"][str(ctx.author.id)]['balance'] += v["amount"]
+            v["amount"] = amount
+            data["users"][str(ctx.author.id)]['balance'] -= amount
+            v["choice"] = "yes"
+            return
+    data["gamba"]["votes"].append({
+        "id": str(ctx.author.id),
+        "name": name,
+        "amount": amount,
+        "choice": "yes",
+    })
+    data["users"][str(ctx.author.id)]['balance'] -= amount
+    await ctx.send(f'```fix\nbet place by {name} for {amount} on yes```')
+
+@bot.command(name='gamba-no', help='Bet on no', enabled = ENABLED)
+async def gamba_no(ctx, amount):
+    name = ctx.author.global_name or ctx.author.name
+    if data["gamba"]["state"] != "betting":
+        await ctx.send('Betting is closed')
+        return
+    if amount == 'all' or amount == 'ALL':
+        amount = float(data["users"][str(ctx.author.id)]['balance'])
+    else:
+        amount = float(amount)
+    if data["users"][str(ctx.author.id)]['balance'] < amount:
+        await ctx.send('Not enough coins')
+        return
+    for v in data["gamba"]["votes"]:
+        if v["name"] == name:
+            data["users"][str(ctx.author.id)]['balance'] += v["amount"]
+            v["amount"] = amount
+            data["users"][str(ctx.author.id)]['balance'] -= amount
+            v["choice"] = "no"
+            return
+    data["gamba"]["votes"].append({
+        "id": str(ctx.author.id),
+        "name": name,
+        "amount": amount,
+        "choice": "no",
+    })
+    data["users"][str(ctx.author.id)]['balance'] -= amount
+    await ctx.send(f'```fix\nbet place by {name} for {amount} on no```')
+
+@bot.command(name='mygamba', help='View your gamba', enabled = ENABLED)
+async def mygamba(ctx):
+    name = ctx.author.global_name or ctx.author.name
+    for v in data["gamba"]["votes"]:
+        if v["name"] == name:
+            await ctx.send(f'```fix\nYou have bet {v["amount"]} on {v["choice"]}```')
+            return
+
+@bot.command(name='gamba-end', help='End gamba', enabled = ENABLED)
+async def end_gamba(ctx, result):
+    if result != "yes" and result != "no":
+        await ctx.send('Invalid result')
+        return
+    if not data["gamba"]:
+        await ctx.send('No gamba in progress')
+        return
+    if data["gamba"]["state"] != "closed":
+        await ctx.send('Betting is still open')
+        return
+    if data["gamba"]["owner"]["id"] != ctx.author.id:
+        await ctx.send('You do not own the gamba')
+        return
+    yes_total = 0
+    no_total = 0
+    for v in data["gamba"]["votes"]:
+        if v["choice"] == "yes":
+            yes_total += v["amount"]
+        else:
+            no_total += v["amount"]
+    if result == "yes":
+        for v in data["gamba"]["votes"]:
+            if v["choice"] == "yes":
+                winnings = ((yes_total / v["amount"]) * no_total)
+                data["users"][v["id"]]['balance'] += v["amount"] + winnings
+                await ctx.send(f'```fix\n{v["name"]} won {winnings}```')
+    else:
+        for v in data["gamba"]["votes"]:
+            if v["choice"] == "no":
+                winnings = ((no_total / v["amount"]) * yes_total)
+                data["users"][v["id"]]['balance'] += v["amount"] + winnings
+                await ctx.send(f'```fix\n{v["name"]} won {winnings}```')
 
 @commands.cooldown(1, 1, commands.BucketType.user)
 @bot.command(name='flip', aliases=['f', 'coin'], help='flip <amount> to bet on a coin. Default amount is 0.', enabled = ENABLED)
-async def flip(ctx, amount: float = 0):
+async def flip(ctx, amount):
+    if amount == 'all' or amount == 'ALL':
+        amount = float(data["users"][str(ctx.author.id)]['balance'])
+    else:
+        amount = float(amount)
     await flip_helper(ctx, data["users"], amount)
 
 @bot.command(name='balance', aliases=['b'], help='Check your balance', enabled = ENABLED)
